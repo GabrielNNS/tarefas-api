46c02b1f4da0e7fb4e0060722f40e4769b2ebd5e
GabrielNNS <gabriel.nascimento33@hotmail.com>
2025-08-22 15:31:14 -0300

    feat/exception-handler

---
+++ src/main/java/com/gabriel/tarefas_api/exception/GlobalExceptionHandler.java
@@ -0,0 +1,20 @@
+package com.gabriel.tarefas_api.exception;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<ErrorResponse> entityNotFound(EntityNotFoundException ex) {
+        ErrorResponse errorResponse = new ErrorResponse(
+                ex.getMessage(),
+                HttpStatus.NOT_FOUND.value()
+        );
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorResponse);
+    }
+}
